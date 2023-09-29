0da452e5fe92500ae7d04574e8a477c2730c5622
AskFanzu <ask.hylleberg@outlook.dk>
2023-09-29 16:48:41 +0200

    more beginning

---
+++ GambleOrDie/GambleOrDie/Games/WordPuzzle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GambleOrDie.Games
+{
+    public class WordPuzzle
+    {
+        int height = 10;
+        int width = 10;
+        public void board(int money)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
