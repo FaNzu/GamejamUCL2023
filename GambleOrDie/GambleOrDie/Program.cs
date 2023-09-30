49ef99866b205f316d1f454bd307b7dab9b27b69
AskFanzu <ask.hylleberg@outlook.dk>
2023-09-30 14:00:20 +0200

    activating program cs

--- GambleOrDie/GambleOrDie/Program.cs
+++ GambleOrDie/GambleOrDie/Program.cs
@@ -1,17 +1,17 @@
-﻿//using GambleOrDie.GameLogic;
-//using GambleOrDie.Model;
+﻿using GambleOrDie.GameLogic;
+using GambleOrDie.Model;
 
-//using GambleOrDie.Games;
+using GambleOrDie.Games;
 
-//namespace GambleOrDie
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Player player = new Player("Player1");
-//            Menu menu = new Menu(player);
-//            menu.StartMenu();
-//        }
-//    }
-//}
+namespace GambleOrDie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Player1");
+            Menu menu = new Menu(player);
+            menu.StartMenu();
+        }
+    }
+}
