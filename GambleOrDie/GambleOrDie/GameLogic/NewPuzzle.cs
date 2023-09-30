cde30333af4e7f1069da5d2d0c0faa20046f57b3
AskFanzu <ask.hylleberg@outlook.dk>
2023-09-30 12:58:49 +0200

    test2

--- GambleOrDie/GambleOrDie/GameLogic/NewPuzzle.cs
+++ GambleOrDie/GambleOrDie/GameLogic/NewPuzzle.cs
@@ -136,7 +136,7 @@ namespace GambleOrDie.GameLogic
 						Console.WriteLine($"Difficulty set to: {difficulty}");
 						WordPuzzle puzzle = new WordPuzzle();
 						stake = BetCoins();
-						puzzle.board();
+						puzzle.board(difficulty);
 						break;
 					case 1:
 						Console.WriteLine("  _   _                 _____               _      \r\n | \\ | |               |  __ \\             | |     \r\n |  \\| | _____      __ | |__) |   _ _______| | ___ \r\n | . ` |/ _ \\ \\ /\\ / / |  ___/ | | |_  /_  / |/ _ \\\r\n | |\\  |  __/\\ V  V /  | |   | |_| |/ / / /| |  __/\r\n |_| \\_|\\___| \\_/\\_/   |_|    \\__,_/___/___|_|\\___|\r\n                                                   \r\n                                                   ");
